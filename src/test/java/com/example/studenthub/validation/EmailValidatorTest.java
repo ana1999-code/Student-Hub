a6ef2a07d880271a61ecd58980e3d4ed664c6c9e
Ana <anavlas1999@gmail.com>
2023-08-29 13:25:25 +0300

    Add tests for email EmailValidator class

--- src/test/java/com/example/studenthub/validation/EmailValidatorTest.java
+++ src/test/java/com/example/studenthub/validation/EmailValidatorTest.java
@@ -1,8 +1,7 @@
 package com.example.studenthub.validation;
 
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -10,18 +9,32 @@ class EmailValidatorTest {
 
     private final EmailValidator emailValidator = new EmailValidator();
 
-    @Test
-    void itShouldReturnTrueWhenValidateCorrectEmail() {
-        String email = "johnsmith@mail.com";
-
+    @ParameterizedTest
+    @CsvSource({
+            "johns@gmail.com",
+            "johns123@gmail.com",
+            "JONS@gmail.com",
+            "Jonnes@gmail.com",
+            "johns@gmail.md",
+            "johns@gmail.softer",
+            "jones_123@gmail.com"
+    })
+    void itShouldValidateCorrectEmail(String email) {
         boolean isValid = emailValidator.test(email);
         assertThat(isValid).isTrue();
     }
 
-    @Test
-    void itShouldReturnFalseWhenValidateIncorrectEmail() {
-        String email = "johnsmithmail.com";
-
+    @ParameterizedTest
+    @CsvSource({
+            "johns@gmail.c",
+            "johns123@gmail.education",
+            "JONSgmail.com",
+            "Jonnes@gmail",
+            "johns!@gmail.md",
+            "johns\"@gmail.softer",
+            "jones_123"
+    })
+    void itShouldNotValidateIncorrectEmail(String email) {
         boolean isValid = emailValidator.test(email);
         assertThat(isValid).isFalse();
     }
